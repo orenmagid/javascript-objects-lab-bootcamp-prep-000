eb5169c7d0ea2094e6b4e3cabf329ad8ddc35a73
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 08:55:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
   recipes[key] = value
   return recipes
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value
+  return recipes
+}
